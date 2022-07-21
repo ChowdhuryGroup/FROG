e13fa838eeb1738dd0ca87eb87c3cef630b49cc4
adam-fisher37 <adf49@case.edu>
2022-07-21 14:00:33 -0400

    changed how data_processing function will take in calibration data from FROG setup.

--- data_processing.py
+++ data_processing.py
@@ -25,15 +25,14 @@ def read_trace(folder,fname,calibration):
 	"""
 	Reads in the raw FROG photo and the calibration data and outputs the raw trace and its associated arrays
 	supports image formats that are supported by cv2.imread, consult documentation if unsure
-	requires calibration data in .txt file, uses numpy.loadtxt
-	format for calibration data is csv and should be of the order:
-	calibration wavelength [m], calibration pixel [pix], delay/pixel [s/pix], wavelength/pix [m/pix]
+	requires calibration data in a dict, order doesnt matter but name and units do, please follow convention
+	calibration wavelength [m] (float), calibration pixel [pix] (int), delay per pixel [s/pix] (float), wavelength per pixel [m/pix] (float)
 	note: horz pix is wvl and vert is delay
 	note: may wish to import data to a dict in future for greater flexiblity
 	inputs:
 	folder - str - relative file path to directory that contains desired FROG photo
 	fname - str - file name of FROG photo you wish to use
-	calibration - str - file name of calibration data that matches FROG photo
+	calibration - dict - dict of calibration data that matches FROG photo, please see format above
 	NOTE: photo and calibration file must be in same directory
 	output:
 	raw_trace - array, NxM - array containing the intensity data from FROG photo, I_FROG(omega_i,tau_j) from Trebino's book
@@ -41,4 +40,23 @@ def read_trace(folder,fname,calibration):
 	trace_wvl - array, Nx1 - array containing the wavelengths recorded by the FROG
 	trace_freq - array, Nx1 - SAA but the frequencies instead
 	"""
-	
+	# assertions for files
+	assert(type(folder)==str), 'input: folder, must be string'
+	assert(type(fname)==str), 'input: fname, must be string'
+	assert(type(calibration)==dict), 'input: calibration, must be a dict'
+	# load in calibration
+	cali_wvl = float(calibration['calibration wavelength']) # [m]
+	cali_pix = int(calibration['calibration pixel']) # [pix]
+	dpp = float(calibration['delay per pixel']) # [s/pix]
+	wpp = float(calibration['wavelength per pixel']) # [m/pix]
+	# load in picture
+	fp = folder+fname
+	try:
+		with open(fp,'r',) as f:
+			raw_pic = cv.imread(f,cv.IMREAD_UNCHANGED)
+	except:
+		raise Exception('error reading picture')
+	# create trace arrays, will need to be able to handle even and odd image dim
+	# create trace delay array
+	if bool(raw_pic.shape[0]%2): # true = odd
+		trc_d = np.linspace((1-raw_pic.shape[0]/2))
